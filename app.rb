d6177e4faa862ca685366ab52ae4902f804501de
Ted Lee <Ted@TedLee.me>
2012-11-25 20:52:19 -0500

    Switch to migration for production

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ end
 configure :production do
 	DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/openpaper")
 	DataMapper.finalize
-	DataMapper.auto_upgrade!
+	DataMapper.auto_migrate!
 end
 
 
